843c139eac5afbded4a66d3541800ec63a53397e
Maguette Diagne <maguette.madiodioudiagne@grtgaz.com>
2026-03-01 15:38:55 +0100

    feat(front): auth pages + routing + header dynamic + landing buttons

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+// @ts-ignore
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import {AuthService} from '../../../shared/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
-export class Login {
-
-}
-
 export class LoginComponent {
-}
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    console.log("SUBMIT déclenché ");
+    if (this.loginForm.valid) {
+
+      this.http.post('http://localhost:5000/login', this.loginForm.value)
+        .subscribe({
+          next: (res: any) => {
+            console.log('Réponse backend :', res);
+
+            // on simule un token pour l'instant
+            this.auth.login('fake-token');
+
+            // redirection vers home
+            this.router.navigate(['/']);
+          },
+          error: (err) => {
+            console.error('Erreur :', err);
+          }
+        });
+
+    }
+  }
+  }
